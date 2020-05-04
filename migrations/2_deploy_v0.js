bc81ee7ae665198134c3fb28cf8424d38b53bd5f
moose-code <jonjonclark@gmail.com>
2020-05-04 09:18:33 +0000

    migrations

--- migrations/2_deploy_v0.js
+++ migrations/2_deploy_v0.js
@@ -2,12 +2,21 @@
 const { scripts, ConfigManager } = require("@openzeppelin/cli");
 const { add, push, create } = scripts;
 
+// Goerli:
 const addressOfLoyalyTokenContract =
-  "0x0000000000000000000000000000000000000000";
+  "0x64Fde89bae3e52b3b853A0ba8F8184D72a398F5b";
 const addressOfWildCardTokenContract =
-  "0x0000000000000000000000000000000000000000";
+  "0x6Da7DD22A9c1B6bC7b2Ba9A540A37EC786E30eA7";
 const addressOfWildCardStewardContract =
-  "0x0000000000000000000000000000000000000000";
+  "0x0C00CFE8EbB34fE7C31d4915a43Cde211e9F0F3B";
+
+// // Mainnet
+// const addressOfLoyalyTokenContract =
+//   "0x231570F1ea43DE41adb3Ee04188CE18c8d10dEED";
+// const addressOfWildCardTokenContract =
+//   "0x6aD0f855c97eb80665F2D0C7d8204895e052C373";
+// const addressOfWildCardStewardContract =
+//   "0x6D47CF86F6A490c6410fC082Fd1Ad29CF61492d0";
 
 async function deploy(options, accounts) {
   add({
@@ -30,7 +39,7 @@ async function deploy(options, accounts) {
   });
 }
 
-module.exports = function (deployer, networkName, accounts) {
+module.exports = function(deployer, networkName, accounts) {
   deployer.then(async () => {
     // Don't try to deploy/migrate the contracts for tests
     if (networkName === "test") {
--- networks.js
+++ networks.js
@@ -1,12 +1,1 @@
-module.exports = {
-  networks: {
-    development: {
-      protocol: 'http',
-      host: 'localhost',
-      port: 8545,
-      gas: 5000000,
-      gasPrice: 5e9,
-      networkId: '*',
-    },
-  },
-};
+truffle-config.js
