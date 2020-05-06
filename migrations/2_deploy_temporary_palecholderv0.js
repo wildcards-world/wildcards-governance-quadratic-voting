83b5965f7c7c008debf291811927396833c1e8a8
Jason Smythe <jason@smythemail.za.net>
2020-05-06 12:54:57 +0200

    add temporary placeholder for the quadratic voting

--- migrations/2_deploy_temporary_palecholderv0.js
+++ migrations/2_deploy_temporary_palecholderv0.js
@@ -2,27 +2,17 @@
 const { scripts, ConfigManager } = require("@openzeppelin/cli");
 const { add, push, create } = scripts;
 
-// Goerli:
+// Mainnet
 const addressOfLoyalyTokenContract =
-  "0x64Fde89bae3e52b3b853A0ba8F8184D72a398F5b";
+  "0x231570F1ea43DE41adb3Ee04188CE18c8d10dEED";
 const addressOfWildCardTokenContract =
-  "0x6Da7DD22A9c1B6bC7b2Ba9A540A37EC786E30eA7";
+  "0x6aD0f855c97eb80665F2D0C7d8204895e052C373";
 const addressOfWildCardStewardContract =
-  "0x0C00CFE8EbB34fE7C31d4915a43Cde211e9F0F3B";
-
-// // Mainnet
-// const addressOfLoyalyTokenContract =
-//   "0x231570F1ea43DE41adb3Ee04188CE18c8d10dEED";
-// const addressOfWildCardTokenContract =
-//   "0x6aD0f855c97eb80665F2D0C7d8204895e052C373";
-// const addressOfWildCardStewardContract =
-//   "0x6D47CF86F6A490c6410fC082Fd1Ad29CF61492d0";
-
-const wildTomorrowFundAddress = "0xFA53ed45C13A2b86daA0074E7AdA739280635d19";
+  "0x6D47CF86F6A490c6410fC082Fd1Ad29CF61492d0";
 
 async function deploy(options, accounts) {
   add({
-    contractsData: [{ name: "WildcardsQV", alias: "WildcardsQV" }],
+    contractsData: [{ name: "PlaceholderContract", alias: "WildcardsQV" }],
   });
 
   await push({ ...options, force: true });
@@ -32,7 +22,7 @@ async function deploy(options, accounts) {
     contractAlias: "WildcardsQV",
     methodName: "initialize",
     methodArgs: [
-      600, //(10 min voting interval)
+      2592000, //(30 days voting interval = 30*24*60*60)
       addressOfLoyalyTokenContract,
       addressOfWildCardTokenContract,
       addressOfWildCardStewardContract,
